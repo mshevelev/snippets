c5994444da7f421e39d524def44e0eceb75f0893
Mikhail Shevelev <shevelev.mikhail@gmail.com>
2024-02-27 18:28:36 -0500

    update to ipynb snippet

--- jupyter/ipynb_clear_output.sh
+++ jupyter/ipynb_clear_output.sh
@@ -1,3 +1,5 @@
 # @tags: bash, jupyter, ipynb, dev, clear_output
 
 jupyter nbconvert --to notebook  --clear-output ../13f.ipynb
+
+find -name "*.ipynb"  -not -path "*/.*" -and -not -path "*mitx*"   -exec jupyter nbconvert --to notebook  --clear-output {} \;
