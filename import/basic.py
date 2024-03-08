2e829c63e6dc37d981e5f70ed2859dd261fda45f
Mikhail Shevelev <shevelev.mikhail@gmail.com>
2024-03-08 09:45:08 -0500

    updated basic.py

--- import/basic.py
+++ import/basic.py
@@ -1,6 +1,6 @@
 # @tags: import, init, basic
 
-import os, sys, shutil, psutil, re, warnings, json, lxml
+import os, sys, shutil, psutil, re, warnings, json
 from glob import glob
 
 
@@ -8,3 +8,16 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 import scipy.stats
+from statsmodels import api as sma
+
+import holoviews as hv
+import hvplot.pandas
+import hvplot.xarray
+
+for local_package in ('/home/mshevelev/git/ms_utils', ):
+  if local_package not in sys.path:
+    sys.path.insert(0, local_package)
+import ms_utils; print(f"{ms_utils.__path__}")
+
+from ms_utils.holoviews.settings import apply_defaults
+apply_defaults()
