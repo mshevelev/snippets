11b7b21a863c205912191694cfbf7a466c7f081e
Mikhail Shevelev <shevelev.mikhail@gmail.com>
2025-01-25 07:44:37 -0500

    added css_styles for jupyter

---
+++ markup/jupyter_css_styles.py
@@ -0,0 +1,18 @@
+# @tags: style, css, html, web, jupyter
+
+from IPython.display import HTML
+
+
+HTML("""
+<style>
+  .theorem {
+    color: red;
+    text-decoration: underline;
+  }
+  .side-comment {
+    color: gray;
+    font-size: 0.8em;
+  }
+</style>
+""")
+
